143bac1821c5575e28740451909e89c0e203ce1d
xovano <xovano@gmail.com>
2018-04-16 02:08:01 +0700

    Add main sources

---
+++ arith.hpp
@@ -0,0 +1,6 @@
+#ifndef ARITH_HPP
+#define ARITH_HPP
+
+int add(int a, int b);
+
+#endif
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include "arith.hpp"
+#include <iostream>
+
+int main() {
+    std::cout << add(2, 40) << std::endl;
+}
+
